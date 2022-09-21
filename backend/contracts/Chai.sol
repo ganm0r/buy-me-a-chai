304d672ddb6c9e5bfd631a2e12c1ce3c2ccb684d
ganm0r <gndhrvmv@gmail.com>
2022-09-21 12:43:12 +0530

    write test contract

---
+++ backend/contracts/Chai.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/hardhat/console.sol";
+
+contract Chai {
+  constructor() payable {
+    console.log('Chai contract!');
+  }
+}
