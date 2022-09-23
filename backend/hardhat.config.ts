65b634a4e3067bcb27ea9ce5ea2257ce786e297e
ganm0r <gndhrvmv@gmail.com>
2022-09-23 11:51:41 +0530

    add goerli testnet to hardhat config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,16 @@
+require("dotenv").config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    }
+  }
 };
 
 export default config;
